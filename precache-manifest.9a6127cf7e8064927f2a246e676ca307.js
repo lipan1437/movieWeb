9b8d027ff0dd3e2ae96df107f7a4f1dd7e9c73a9
Lipan Padhan <lipanpadhan1432@gmail.com>
2023-11-07 10:11:25 +0530

    Updates

---
+++ precache-manifest.9a6127cf7e8064927f2a246e676ca307.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011ae2f66d6dd9aba1d5cd53eb503617",
+    "url": "/moviebrowser/index.html"
+  },
+  {
+    "revision": "a6c05b20e858d752eba8",
+    "url": "/moviebrowser/static/css/main.bda53660.chunk.css"
+  },
+  {
+    "revision": "39f0c0e535057a2e54c3",
+    "url": "/moviebrowser/static/js/2.03e1aae1.chunk.js"
+  },
+  {
+    "revision": "a6c05b20e858d752eba8",
+    "url": "/moviebrowser/static/js/main.a2afc2fc.chunk.js"
+  },
+  {
+    "revision": "24da519c5d4289d25da5",
+    "url": "/moviebrowser/static/js/runtime~main.4a9284b1.js"
+  }
+]);
